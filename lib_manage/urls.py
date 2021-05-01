b1e75c58fdcedd6346e4045e57b113ae1f91c162
NHSanto <nahid170053@diit.edu.bd>
2021-05-02 05:56:13 +0600

    Added add_book and add_item form

--- lib_manage/urls.py
+++ lib_manage/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import HomePage,Book_Checkout, Reservation, Success,Reservation,SearchResultsListView
+from .views import HomePage,Book_Checkout, Reservation, Success,Reservation,SearchResultsListView,AddBook,AddItem
 urlpatterns = [
     path('', HomePage, name='home'),
     path('book_checkout',Book_Checkout,name='book_checkout'),
     path('reservation/<uuid:id>',Reservation,name='reservation'),
     path('success',Success,name='success'),
-    path('search', SearchResultsListView.as_view(), name ='search_results')
+    path('search', SearchResultsListView.as_view(), name ='search_results'),
+    path('addbook',AddBook , name='add_book_detail'),
+    path('additem',AddItem, name='add_item'),
 ]
